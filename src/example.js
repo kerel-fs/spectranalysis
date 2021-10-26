d22bbfee4cf999fdccf2f984cd44a8b37b063494
Fabian P. Schmidt <kerel@mailbox.org>
2021-10-26 11:25:55 +0300

    Add hyperlinks to source code in footer
    Signed-off-by: Fabian P. Schmidt <kerel@mailbox.org>

--- src/example.js
+++ src/example.js
@@ -3,7 +3,8 @@ import { EasyCloning } from './easy.js';
 var myEasyCloning =  new EasyCloning('#spectros', document.location.hash.substring(1));
 
 // Show version in footer
+const spectroplot_hash = __DEP_SPECTROPLOT__.split('#')[1];
 document.getElementById('footer_version').style.display = null;
-document.getElementById('footer_version').innerHTML = '<span>kerel-fs/spectranalyis#' + __COMMIT_HASH__ +
-                                                      '</span>, <span>'+ __DEP_SPECTROPLOT__ +
+document.getElementById('footer_version').innerHTML = '<span><a href="https://github.com/kerel-fs/spectranalysis">kerel-fs/spectranalyis</a>@' + __COMMIT_HASH__ +
+                                                      '</span>, <span><a href="https://github.com/kerel-fs/spectroplot-js">kerel-fs/spectroplot.js</a>@'+ spectroplot_hash +
                                                       '</span>';
